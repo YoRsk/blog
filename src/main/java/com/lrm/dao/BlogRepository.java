fac064c70f8b36be1ea353b439a3c888541c10c4
liuyi <782527347@qq.com>
2024-02-18 22:59:04 +0900

    update for rerunning

--- src/main/java/com/lrm/dao/BlogRepository.java
+++ src/main/java/com/lrm/dao/BlogRepository.java
@@ -21,7 +21,7 @@ public interface BlogRepository extends JpaRepository<Blog, Long>, JpaSpecificat
     Page<Blog> findByQuery(String query,Pageable pageable);
 
 
-    @Transactional
+    @Transactional //如果在执行过程中遇到异常，则回滚事务。这保证了被注解方法中的所有数据库操作要么全部成功，要么在遇到错误时全部回滚，从而保证数据的一致性。
     @Modifying
     @Query("update Blog b set b.views = b.views+1 where b.id = ?1")
     int updateViews(Long id);
