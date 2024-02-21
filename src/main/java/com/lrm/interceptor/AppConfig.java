e89e093d3cfd27a35c3df6e82edcfca25dc3b474
liuyi <782527347@qq.com>
2024-02-21 22:14:01 +0900

    start to for SpringCloud microservices

---
+++ src/main/java/com/lrm/interceptor/AppConfig.java
@@ -0,0 +1,20 @@
+package com.lrm.interceptor;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+    /*
+    * This is a configuration class that creates a RestTemplate bean.
+    * The @LoadBalanced annotation tells Spring Cloud to create a load-balanced RestTemplate.
+    * it will understand the services id for Eureka to use Ribbon to load balance the requests
+     */
+    @LoadBalanced
+    @Bean // spring will manage the lifecycle of this bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
